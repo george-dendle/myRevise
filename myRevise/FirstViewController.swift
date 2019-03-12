66998a29b80613fbbd995c7b3b1120597605ab1a
George Dendle <george.dendle@gymamigo.com>
2019-03-12 11:49:43 +0000

    Initial Commit

---
+++ myRevise/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  myRevise
+//
+//  Created by George Dendle on 12/03/2019.
+//  Copyright © 2019 Reeds_School. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ myRevise/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  myRevise
+//
+//  Created by George Dendle on 12/03/2019.
+//  Copyright © 2019 Reeds_School. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
